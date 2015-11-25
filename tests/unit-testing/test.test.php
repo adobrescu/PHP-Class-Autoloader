9e656909a8257eff9b0466fad1a44ae826f4fcee
Adi Dobrescu <adobrescu@insolitservices.ro>
2015-11-25 13:59:03 +0200

    Test file fixes

--- tests/unit-testing/test.test.php
+++ tests/unit-testing/test.test.php
@@ -9,18 +9,18 @@ $sourceFileNames=$am->debugGetSourceFileNames();
 
 $this->ASSERT_TRUE(in_array( '../test-classes/SampleClass.class.php', $sourceFileNames));
 $this->ASSERT_TRUE(in_array( '../test-classes/Sources.php', $sourceFileNames));
-$this->ASSERT_TRUE(in_array( '../test-classes/lib/Class1.class.php', $sourceFileNames));	
-$this->ASSERT_TRUE(in_array( '../test-classes/lib/sub-lib/Sources.php', $sourceFileNames));
+$this->ASSERT_TRUE(in_array( '../test-classes/lib/Class2.class.php', $sourceFileNames));	
+$this->ASSERT_TRUE(in_array( '../test-classes/lib/sub-lib/OtherSources.php', $sourceFileNames));
 
 $declaredClasses=$am->debugGetDeclaredClasses();
 
 $this->ASSERT_EQUALS($declaredClasses['SampleNS\SampleClass'], '../test-classes/SampleClass.class.php');
 $this->ASSERT_EQUALS($declaredClasses['TestClass'], '../test-classes/Sources.php');
 $this->ASSERT_EQUALS($declaredClasses['TestClass2'], '../test-classes/Sources.php');
-$this->ASSERT_EQUALS($declaredClasses['C1'], '../test-classes/lib/Class1.class.php');
-$this->ASSERT_EQUALS($declaredClasses['NS2\SubClass'], '../test-classes/lib/sub-lib/Sources.php');
+$this->ASSERT_EQUALS($declaredClasses['C2'], '../test-classes/lib/Class2.class.php');
+$this->ASSERT_EQUALS($declaredClasses['NS2\SubClass'], '../test-classes/lib/sub-lib/OtherSources.php');
 
 $obj=new \SampleNS\SampleClass();
-echo "\n".get_class($obj)."\n";
+
 
 
