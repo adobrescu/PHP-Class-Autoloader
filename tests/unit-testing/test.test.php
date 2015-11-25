e19e3da2d27a565332d3abc5fd6a8ac233d7209a
Adi Dobrescu <adobrescu@insolitservices.ro>
2015-11-25 12:11:22 +0200

    Assert on autoload

--- tests/unit-testing/test.test.php
+++ tests/unit-testing/test.test.php
@@ -12,7 +12,15 @@ $this->ASSERT_TRUE(in_array( $sourcesDir.'/Sources.php', $sourceFileNames));
 $this->ASSERT_TRUE(in_array( $sourcesDir.'/lib/Class1.class.php', $sourceFileNames));	
 $this->ASSERT_TRUE(in_array( $sourcesDir.'/lib/sub-lib/Sources.php', $sourceFileNames));
 
-		
+$declaredClasses=$am->debugGetDeclaredClasses();
 
-print_r($sourceFileNames);
+$this->ASSERT_EQUALS($declaredClasses['SampleNS\SampleClass'], '../test-classes/SampleClass.class.php');
+$this->ASSERT_EQUALS($declaredClasses['TestClass'], '../test-classes/Sources.php');
+$this->ASSERT_EQUALS($declaredClasses['TestClass2'], '../test-classes/Sources.php');
+$this->ASSERT_EQUALS($declaredClasses['C1'], '../test-classes/lib/Class1.class.php');
+$this->ASSERT_EQUALS($declaredClasses['NS2\SubClass'], '../test-classes/lib/sub-lib/Sources.php');
+
+$obj=new \SampleNS\SampleClass();
+echo "\n".get_class($obj)."\n";
+
 
