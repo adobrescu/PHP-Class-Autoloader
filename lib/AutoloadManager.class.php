d60773332726269cd70e5b3fcb6e8b1d52d9d99d
Adi Dobrescu <adobrescu@insolitservices.ro>
2015-11-25 10:34:24 +0200

    Adds empty protected constructor and public static method to create/return the unique AM instance (singleton)

--- lib/AutoloadManager.class.php
+++ lib/AutoloadManager.class.php
@@ -2,4 +2,19 @@
 
 class AutoloadManager
 {
+	static protected $___instance;
+	
+	protected function __constructor()
+	{
+	}
+	
+	static public function ___getInstance()
+	{
+		if(!static::$___instance)
+		{
+			static::$___instance=new static();
+		}
+		
+		return static::$___instance;
+	}
 }
