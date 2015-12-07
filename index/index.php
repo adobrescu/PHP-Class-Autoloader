a677b1ad2303376b92d74cf6b34337afdada0225
Adi Dobrescu <adobrescu@insolitservices.ro>
2015-12-07 12:02:06 +0200

    Tests scanning by file extensions

--- index/index.php
+++ index/index.php
@@ -1,14 +1,18 @@
 <?php
 
-include_once(__DIR__.'/lib/ClassAutoloader.class.php');
+include_once(__DIR__.'/../lib/ClassAutoloader.class.php');
 
 echo '<pre>';
 echo '<br><br>Scan '.realpath(__DIR__.'/test-classes').'.<br><br>Paths are relative to the directory where the file that defines ClassAutoloader is stored:<br><br>';
 $ca=ClassAutoloader::___getInstance(
-		array(__DIR__.'/test-classes'),
-		__DIR__.'/class-autoloader-config.php',
+		array(__DIR__.'/../test-classes/'),
+		__DIR__.'/../class-autoloader-config.php',
 		true
-		
+		,
+		array(__DIR__.'/../test-classes/lib/sub-lib/backup'),
+		array('php'),
+		array('class.php')
 		);
+
 print_r($ca->debugGetDeclaredClasses());
 
